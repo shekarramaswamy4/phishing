a2522ff7f6e597c0420ff63053eaeeebe40c7e7e
shekarramaswamy4 <shekar.ram.428@gmail.com>
2021-09-26 14:33:04 -0700

    fixed type

--- ts/popup.ts
+++ ts/popup.ts
@@ -11,13 +11,13 @@ changeColor.addEventListener("click", async () => {
 
   chrome.scripting.executeScript({
     target: { tabId: tab.id },
-    function: setPageBackgroundColor,
+    func: setPageBackgroundColor,
   });
 });
 
 // The body of this function will be executed as a content script inside the
 // current page
-function setPageBackgroundColor() {
+const setPageBackgroundColor = () => {
   chrome.storage.sync.get("color", ({ color }) => {
     document.body.style.backgroundColor = color;
   });

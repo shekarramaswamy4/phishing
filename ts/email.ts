465d42f79e37e4e6e5fd47537c5b6d3dad026881
shekarramaswamy4 <shekar.ram.428@gmail.com>
2021-09-29 22:06:17 -0700

    progress

--- ts/email.ts
+++ ts/email.ts
@@ -1,22 +1,61 @@
-window.addEventListener('hashchange', () => highlightSketchyLinks())
+window.addEventListener('hashchange', () => modifyLinks())
+
+const urlRegex = /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+(\.[a-z]{2,}){1,3}(#?\/?[a-zA-Z0-9#]+)*\/?(\?[a-zA-Z0-9-_]+=[a-zA-Z0-9-%]+&?)?$/
 
 function isLinkSketchy(link: HTMLAnchorElement): boolean {
-  return true
+  const textHost = getHost(link.text) 
+  const hrefHost = getHost(link.href)
+  if (textHost != hrefHost) {
+    return true
+  }
+  return false 
 }
 
-function highlightSketchyLinks() {
+function getHost(href: string): string {
+  return Object.assign(document.createElement('a'), { href }).host
+}
+
+
+function shouldIgnoreLink(link: HTMLAnchorElement): boolean {
+  if (link.href.includes(".google.com")) {
+    return true 
+  }
+  if (link.text === "") {
+    return true
+  }
+  return false 
+}
+
+function modifyLinks() {
   const elements = document.getElementsByTagName("a")
   console.log('beginning to highlight sketchy links')
   // @ts-ignore
   for (const e of elements) {
-    if (isLinkSketchy(e)) {
-      // highlight a tag
+
+    if (shouldIgnoreLink(e)) continue
+
+    const href = e.href
+    const text = e.text
+
+ 
+    // TODO: add some sort of uniformization of links so silly link rewriting doesn't happen
+    // example, https://google.com doesn't need to be rewritten to google.com.
+    e.addEventListener('mouseover', function() {
+      e.text = href
+    })
+    e.addEventListener('mouseleave', function() {
+      e.text = text
+    })
+
+    if (urlRegex.test(text) && isLinkSketchy(e)) {
+      e.style.color = "red"
     }
   }
 }
 
 // Desired functionality
-// 1. highlight "sketchy" links (points to somewhere else)
-// 1a. potentially show where hyperlinked text resolves to
+// 1. highlight "sketchy" links (points to somewhere else) [done]
+// 1a. show where hyperlinked text resolves to [done]
 // 2. Notify if the sender is not from your org
 // 3. highlight known malicious sites
+// 4. don't highlight links in composer?

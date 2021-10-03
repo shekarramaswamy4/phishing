b6c421a3e520f299e16cbd620ed5d3e2765b7d39
shekarramaswamy4 <shekar.ram.428@gmail.com>
2021-10-02 18:02:20 -0700

    highlight facebook ads

--- ts/facebook.ts
+++ ts/facebook.ts
@@ -1,2 +1,15 @@
-window.addEventListener('mousemove', () => highlightSponsored())
+window.addEventListener('mousemove', () => highlightSuggested())
+window.addEventListener('scroll', () => highlightSuggested())
 
+function highlightSuggested() {
+  const elements = document.getElementsByTagName("span")
+
+  // @ts-ignore
+  for (const e of elements) {
+    const text = e.innerText.toLowerCase()
+    if (text.includes("sponsored") || text.includes("suggested for you")) {
+      e.style.color = "red"
+      e.style.border = "1px solid red"
+    }
+  }
+}

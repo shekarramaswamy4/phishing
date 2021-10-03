592c6b0c47df7d3d2a98ce101ac9d005dbfa83d6
shekarramaswamy4 <shekar.ram.428@gmail.com>
2021-10-02 18:10:57 -0700

    highlight facebook ads

--- ts/facebook.ts
+++ ts/facebook.ts
@@ -2,10 +2,18 @@ window.addEventListener('mousemove', () => highlightSuggested())
 window.addEventListener('scroll', () => highlightSuggested())
 
 function highlightSuggested() {
-  const elements = document.getElementsByTagName("span")
-
+  const divs = document.getElementsByTagName("div")
   // @ts-ignore
-  for (const e of elements) {
+  for (const e of divs) {
+    const classList = e.classList.value
+    if (classList.includes("sponsored_ad")) {
+      e.remove()
+    }
+
+
+  const spans = document.getElementsByTagName("span")
+  // @ts-ignore
+  for (const e of spans) {
     const text = e.innerText.toLowerCase()
     if (text.includes("sponsored") || text.includes("suggested for you")) {
       e.style.color = "red"
@@ -13,3 +21,10 @@ function highlightSuggested() {
     }
   }
 }
+
+/*
+ * Ideas to block FB ads:
+ * - find closest div to sponsored text that takes up a reasonable amount of space (vs. whole page)
+ * -> use scrollHeight and scrollWidth
+ * sponsored_ad in div class?
+ */
